74dc1d63271bdecd729024e2c73d45a09efaffcc
CodeRi13 <ruto1924@gmail.com>
2017-08-13 10:38:11 +0900

    Modify show gitignore types option to -l or --list

--- bin/gitignator.js
+++ bin/gitignator.js
@@ -3,14 +3,12 @@ const lib = require('../lib');
 
 commander
   .version('1.0.0')
-  .option('-t, --types', 'Show gitignore types', false)
-  .option('-g, --global <items>', 'Switch global gitignore types', false)
+  .option('-l, --list', 'Show gitignore types', false)
+  .option('-g, --global', 'Switch global gitignore types', false)
   .parse(process.argv);
 
-const showTypes = commander.types;
+const showTypeList = commander.list;
 const switchGlobal = commander.global;
 
-console.log(commander.global);
-
-lib(showTypes, switchGlobal)
-  .catch(console.error);
+lib(showTypeList, switchGlobal)
+  .catch(err => console.error(err));
