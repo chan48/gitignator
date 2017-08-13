487c262c73ba26b4fe1962108159771623b6eefe
CodeRi13 <ruto1924@gmail.com>
2017-08-13 15:18:32 +0900

    Add default command print

--- bin/gitignator.js
+++ bin/gitignator.js
@@ -6,7 +6,7 @@ const lib = require('../lib');
 commander
   .version('1.0.0')
   .option('-g, --global', 'Switch gitignore to global template')
-  .option('-r, --renew', 'Remove all contents in .gitignore before write gitignore templates');
+  .option('-r, --renew', 'Remove all contents in .gitignore before write gitignore templates (only for create command)');
 
 commander
   .command('list')
@@ -19,3 +19,7 @@ commander
   .action(ignores => lib.createIgnore(commander.global, ignores, commander.renew));
 
 commander.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  commander.outputHelp();
+}
